91a974925845d1423c49127c93e55a5568f00c8b
Tino Didriksen <mail@tinodidriksen.com>
2024-09-02 13:49:25 +0200

    macOS find doesn't have -executable; Ensure dir

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-find * -name '*.py' -executable | xargs -n1 -IX bash -c './X >/dev/null'
+DIR=$(cd $(dirname $0);pwd)
+cd "$DIR"
+
+ls -1 lg2/*.py | xargs -n1 -IX bash -c 'echo X; ./X >/dev/null'
 rsync -avz lg2/2*x.sqlite kal@learn.gl:public_html/online-dev/d/lg2/sentence/2x/
