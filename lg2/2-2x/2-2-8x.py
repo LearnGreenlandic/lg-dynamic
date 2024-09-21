8c74affd1db7096c52decbc6a92c4fdaea12bb4c
Tino Didriksen <mail@tinodidriksen.com>
2024-09-21 16:09:23 +0200

    Snip 2.2.8x; Rename 2.2.9x -> 2.2.8x

--- lg2/2-2x/2-2-8x.py
+++ lg2/2-2x/2-2-8x.py
@@ -11,24 +11,38 @@ import shared as S
 load_corpus('2-2x-corpus.txt')
 
 S.patterns.append([
-	sfx(C() | Grep(r'(^(kalaaleq|qallunaaq)|(Sem/(Fem|Mask|Hprof))).*Abs\+Sg\+UNA\b') | Inv(r'\+INNAQ') | Inv(r'\+(LI|LU)\b'), '\t@SUBJ>'),
-	sfx(C() | Grep(r'^(aallar|ajor|ilinniar|piareer|pikkorip|pizza.*TUR|whisky.*TUR).*Par\+3Sg') | Inv(r'\+(TUQ\+U|MIU|LIRI|INNAQ|PAK\+SUAQ\+QAR\+Sem/have\+LAAR)') | Inv(r'ilinniarnertuunngorniarfik|ilinniartitsisoq') | Inv(r'\+(LI|LU|UNA)\b'), '\t@PRED'),
-	])
-S.patterns.append([
-	sfx(C() | Grep(r'^uanga.*Abs\+1Sg\+UNA\b') | Inv(r'\+INNAQ') | Inv(r'\+(LI|LU)\b'), '\t@SUBJ>'),
-	sfx(C() | Grep(r'^(aallar|ajor|ilinniar|piareer|pikkorip|pizza.*TUR|whisky.*TUR).*Par\+1Sg') | Inv(r'\+(TUQ\+U|MIU|LIRI|INNAQ|PAK\+SUAQ\+QAR\+Sem/have\+LAAR)') | Inv(r'ilinniarnertuunngorniarfik|ilinniartitsisoq') | Inv(r'\+(LI|LU|UNA)\b'), '\t@PRED'),
+	sfx(C() | Grep(r'Sem/inst.*Lok') | Inv(r'\+(INNAQ|3PlPoss)') | Inv(r'\+(LI|LU|UNA)\b'), '\t@ADVL>'),
+	sfx(C() | Grep(r'(^(ilinniar\+|suli)|(Sem/(food|drink-h|drink)\+TUR)).*Ind\+1Sg') | Inv(r'\+(LI|LU|UNA)\b'), '\t@PRED'),
+	['⇒\t⇒\t⇒'], # '⇒' is U+21D2 Rightwards Double Arrow
+	['P1\tP1\t@ADVL>'],
+	['P2\tP2\t@OBJ>'],
+	sfx(C() | Grep(r'^TA\+una\+Pron\+Abs\+Sg') | Inv(r'\+(LI|LU|UNA)\b'), '\t@N<'),
+	sfx(C() | Grep(r'^(pi\+Sem/relate\+V\+|eqqaama\+).*Ind\+1Sg\+3SgO') | Inv(r'\+(TAR|SSA|TARIAQAR)\+') | Inv(r'\+(LI|LU|UNA)\b'), '\t@PRED'),
 	])
 
 cartesian()
 
 Qs = []
+def q(sentence):
+	Q = []
+	for w in sentence:
+		w = w.split('\t')
+		if w[0] == 'P1':
+			w = sentence[0].split('\t')
+		elif w[0] == 'P2':
+			w[0] = sentence[1].split('\t')[0].replace('+V+Ind+1Sg', '+NIQ+N+Abs+Sg+1SgPoss')
+			if w[0] not in S.corpus_kv:
+				return
+			w[1] = S.corpus_kv[w[0]]
+		Q.append(w)
+	Qs.append(Q)
+
 for sentence in S.sentences:
-	q = [w.split('\t') for w in sentence]
-	Qs.append(q)
+	q(sentence)
 
 for q in Qs:
 	# Human-readable debug print of generated sentences
-	q = ucfirst(' '.join([w[1] for w in q])) + '!'
+	q = ucfirst(' '.join([w[1] for w in q])) + '.'
 	print(f'{q}')
 
 write_qs(Qs)
