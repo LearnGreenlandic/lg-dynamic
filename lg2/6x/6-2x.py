813702b7fcdce810ae908ffc5d15bed99dcd3d5f
Tino Didriksen <mail@tinodidriksen.com>
2025-03-31 18:25:46 +0200

    Fix LG2 6-2x

--- lg2/6x/6-2x.py
+++ lg2/6x/6-2x.py
@@ -13,8 +13,15 @@ load_corpus('6x-corpus.txt')
 S.patterns.append([
 	sfx(C() | Grep(r'(ippassaq|taamani|ullumi$)') | Inv(r'Sem/dur'), '\t@ADVL>'),
 	sfx(C() | Grep(r'Sem/(Geo|inst).*IP\+V\+Cau\+1Sg') | Inv(r'Sem/inst.*(KKUT|SSAQ|INNAQ|PAK)'), '\t@CAU>'),
-	sfx(C() | Grep(r'Sem/(Fem|Mask)\+Prop\+(Abs|Rel)\+Sg'), '\t@SUBJ>'),
+	sfx(C() | Grep(r'Sem/(Fem|Mask)\+Prop\+Rel\+Sg'), '\t@SUBJ>'),
 	sfx(C() | Grep(r'(encounter|cross|leave).*Ind\+3Sg\+1SgO') | Inv(r'(TAQ|TAR|VALLAAR|SSA|NIQAR)'), '\t@PRED'),
+	['.\t.\t@CLB'],
+	])
+S.patterns.append([
+	sfx(C() | Grep(r'(ippassaq|taamani|ullumi$)') | Inv(r'Sem/dur'), '\t@ADVL>'),
+	sfx(C() | Grep(r'Sem/(Geo|inst).*IP\+V\+Cau\+1Sg') | Inv(r'Sem/inst.*(KKUT|SSAQ|INNAQ|PAK)'), '\t@CAU>'),
+	sfx(C() | Grep(r'Sem/(Fem|Mask)\+Prop\+Abs\+Sg'), '\t@SUBJ>'),
+	sfx(C() | Grep(r'(encounter|cross|leave).*Ind\+1Sg\+3SgO') | Inv(r'(TAQ|TAR|VALLAAR|SSA|NIQAR)'), '\t@PRED'),
 	['.\t.\t@CLB'],
 	])
 S.patterns.append([
