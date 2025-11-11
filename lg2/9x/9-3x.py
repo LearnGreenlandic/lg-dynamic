52d7ec7c29dc409b3d3a9bb5463c7a6f65a0a684
planggard <pela@oqaasileriffik.gl>
2025-11-11 11:14:12 +0100

    Rettet kongruensfejl i 9-3x

--- lg2/9x/9-3x.py
+++ lg2/9x/9-3x.py
@@ -12,11 +12,18 @@ load_corpus('9x-corpus.txt')
 
 S.patterns.append([
 	sfx(C() | Grep(r'Sem/(remember|think|assume|say|comprehend)\+V\+Ind\+1(Sg|Pl)\+3SgO'), '\t@PRED'),
-	sfx(C() | Grep(r'(Sem/(Hfam|Hprof)(\+KU)?\+N\+Abs\+Pl\+1SgPoss\s|(Mask|Fem)\+Prop\+Abs)') | Inv(r'(angajoqqaaq|ilinniarnertooq|ilinniartoq|oqaasilerisoq|sakkutooq|tusagassiortoq|eqqumiitsuliortoq)'), '\t@OBJ>'),
+	sfx(C() | Grep(r'(Sem/(Hfam|Hprof)(\+KU)?\+N\+Abs\+Pl\+1SgPoss\s)') | Inv(r'(angajoqqaaq|ilinniarnertooq|ilinniartoq|oqaasilerisoq|sakkutooq|tusagassiortoq|eqqumiitsuliortoq|aappaq|anaana|ataata|nuliaq|ui|marluliaq|kunngi)'), '\t@OBJ>'),
+	sfx(C() | Grep(r'Prop\+Lok\+(Sg|Pl)\s') | Inv(r'Sem/inst\+KKUT') | Inv(r'(Tusagassiornermik|Ilisimatusarfik).*Pl\s'), '\t@ADVL>'),
+	sfx(C() | Grep(r'Sem/(cross|require|socialize|teach|encounter|serve|see)\+(SSA|NIAR\+Sem/plan).*(Cont|ContNeg)\+3PlO') | Inv(r'GALUAR'), '\t@CL-<CIT'),
+	['.\t.\t@CLB'],
+	])
+S.patterns.append([
+	sfx(C() | Grep(r'Sem/(remember|think|assume|say|comprehend)\+V\+Ind\+1(Sg|Pl)\+3SgO'), '\t@PRED'),
+	sfx(C() | Grep(r'((Mask|Fem)\+Prop\+Abs)'), '\t@OBJ>'),
 	sfx(C() | Grep(r'Prop\+Lok\+(Sg|Pl)\s') | Inv(r'Sem/inst\+KKUT') | Inv(r'(Tusagassiornermik|Ilisimatusarfik).*Pl\s'), '\t@ADVL>'),
 	sfx(C() | Grep(r'Sem/(cross|require|socialize|teach|encounter|serve|see)\+(SSA|NIAR\+Sem/plan).*(Cont|ContNeg)\+3SgO') | Inv(r'GALUAR'), '\t@CL-<CIT'),
 	['.\t.\t@CLB'],
-	])
+	])	
 S.patterns.append([
 	sfx(C() | Grep(r'^(.*Sem/(Fem|Mask)\+Prop|ilinniarnertooq\+Sem/Hprof\+N|ilinniartoq\+Sem/Hprof\+N|oqaasilerisoq\+Sem/Hprof\+N|sakkutooq\+Sem/Hprof\+N|tusagassiortoq\+Sem/Hprof\+N|eqqumiitsuliortoq\+Sem/Hprof\+N)\+Rel\+Sg') | Inv(r'Poss'), '\t@SUBJ>'),
 	sfx(C() | Grep(r'Sem/(remember|think|assume|say|comprehend)\+V\+Ind\+3Sg\+3SgO'), '\t@PRED'),
